7a9e7cbbb8daa507732cac8a4fb73f0c2d4b688f
DHgit1 <33430196+DHgit1@users.noreply.github.com>
2018-11-30 12:20:57 -0800

    First commit

---
+++ src/main/java/edu/csumb/cst438/FinalizeService/FinalizeServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.csumb.cst438.FinalizeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalizeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalizeServiceApplication.class, args);
+	}
+}
