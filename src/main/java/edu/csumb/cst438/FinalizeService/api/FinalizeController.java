57bb0f5d955511aed22d989902a27ff0c0d22da2
DHgit1 <33430196+DHgit1@users.noreply.github.com>
2018-12-01 13:40:17 -0800

    Testing Heroku

--- src/main/java/edu/csumb/cst438/FinalizeService/api/FinalizeController.java
+++ src/main/java/edu/csumb/cst438/FinalizeService/api/FinalizeController.java
@@ -20,7 +20,13 @@ import edu.csumb.cst438.finalizeservice.api.products.ProductName;
 import edu.csumb.cst438.finalizeservice.api.products.Stock;
 import edu.csumb.cst438.finalizeservice.business.Manager;
 
-
+/* TODO:
+    When user presses “Confirm Purchase”:
+    confirms the User has sufficient “credit” to make the purchase, 
+    confirms that the purchase can be made given the # of items in stock, 
+    and appropriately decrements the user’s “credit” and the “product stock” (in the backend) 
+    before forwarding the user to a confirmation page if all was successful.
+*/
 @RestController
 public class FinalizeController {
     @Autowired
@@ -29,20 +35,20 @@ public class FinalizeController {
     @GetMapping ("/Products")
     @ResponseBody
     List<Product> getProducts () {
-        Product Catnip = new Product("1", new ProductName("Catnip"), new Description("Catnip for cats"), new Image(""), new Price(10), new Stock(5));
-        Product Collar = new Product("2", new ProductName("Collar"), new Description("Collar for a cat"), new Image(""), new Price(10), new Stock(5));
-        Product Kong = new Product("3", new ProductName("Kong"), new Description("Toy for dogs"), new Image(""), new Price(10), new Stock(5));
-        List<Product> result = Arrays.asList(Catnip, Collar, Kong);
-        // List<Hero> result = callDB();
+        // Product Catnip = new Product("1", new ProductName("Catnip"), new Description("Catnip for cats"), new Image(""), new Price(10), new Stock(5));
+        // Product Collar = new Product("2", new ProductName("Collar"), new Description("Collar for a cat"), new Image(""), new Price(10), new Stock(5));
+        // Product Kong = new Product("3", new ProductName("Kong"), new Description("Toy for dogs"), new Image(""), new Price(10), new Stock(5));
+        // List<Product> result = Arrays.asList(Catnip, Collar, Kong);
+        List<Product> result = callDB();
         return result;
     }
 
-    // private List<Hero> callDB () {
-    //     String uri = "http://localhost:8081/allHeroes";
-    //     RestTemplate restTemplate = new RestTemplate();
-    //     ResponseEntity<List<Hero>> result = restTemplate.exchange(uri,
-    //     HttpMethod.GET,null, 
-    //     new ParameterizedTypeReference<List<Hero>>(){});
-    //     return result.getBody();
-    // }
+    private List<Product> callDB () {
+        String uri = "https://productsdb-service.herokuapp.com/allProducts";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<Product>> result = restTemplate.exchange(uri,
+        HttpMethod.GET,null, 
+        new ParameterizedTypeReference<List<Product>>(){});
+        return result.getBody();
+    }
 }
